b1bbea59687c2a111d046896a5386c8e13be9386
haiqin.chen <haiqin.chen@hiscene.com>
2021-11-12 15:58:31 +0800

    add:common module

--- app/src/main/java/com/wilson/common/MainActivity.kt
+++ app/src/main/java/com/wilson/common/MainActivity.kt
@@ -2,10 +2,13 @@ package com.wilson.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.wilson.base.TestApi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.text).text = TestApi.transferStr("from data in")
     }
 }
---
+++ base/src/main/java/com/wilson/base/TestApi.kt
@@ -0,0 +1,14 @@
+package com.wilson.base
+
+/**
+ *
+ * @author wilson
+ * @date 12/11/2021
+ * Email: haiqin.chen@hiscene.com
+ */
+object TestApi {
+
+    fun transferStr(dataInt:String):String{
+        return "afterTransfer:${dataInt}"
+    }
+}
